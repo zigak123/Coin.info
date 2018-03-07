5c71ca9b34d775414cb1408e878851ba10a2469d
ziga kus <ziga.kus@gmail.com>
2018-03-07 01:32:21 +0100

    aded save function to user schema

--- userSchema.js
+++ userSchema.js
@@ -21,5 +21,21 @@ var UserSchema = new mongoose.Schema({
     required: true,
   }
 });
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+
+
+
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
