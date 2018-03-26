574ea0d3e1adc8fca8f6abca9fba7c8a96923a28
ziga kus <ziga.kus@gmail.com>
2018-03-26 02:05:15 +0200

    coin details component, replaced chart.js API, created new coinDetails state

--- public/scripts/coinDetailsComponent.js
+++ public/scripts/coinDetailsComponent.js
@@ -1,7 +1,133 @@
 app.component('coinDetails',{
     bindings: { coin_data: '<'},
 templateUrl: '/public/templates/coinDetails.html',
-controller: function ($scope, $stateParams) {
-    
+controller: function ($scope, $stateParams, $http, tickerSrv) {
+	console.log($stateParams.coin_data)
+   $http({
+    method : "GET",
+    url : "https://min-api.cryptocompare.com/data/histoday?fsym="+$stateParams.coin_data.Symbol+"&tsym=USD&limit=30"
+  }).then(function(response) {
+     
+
+      var panelCall =  function (data) {
+           console.log("haha")
+         }
+
+
+      tickerSrv.subscribe($stateParams.coin_data.Symbol, panelCall);
+    }, function myError(response) {
+      console.log(response);
+  });
+
+
+
+
+
+    var chart = AmCharts.makeChart( "chartdiv", {
+  "type": "serial",
+  "theme": "dark",
+  "dataDateFormat":"YYYY-MM-DD",
+  "valueAxes": [ {
+    "position": "left"
+  } ],
+  "graphs": [ {
+    "id": "g1",
+    "balloonText": "Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>",
+    "closeField": "close",
+    "fillColors": "#7f8da9",
+    "highField": "high",
+    "lineColor": "#7f8da9",
+    "lineAlpha": 1,
+    "lowField": "low",
+    "fillAlphas": 0.9,
+    "negativeFillColors": "#db4c3c",
+    "negativeLineColor": "#db4c3c",
+    "openField": "open",
+    "title": "Price:",
+    "type": "candlestick",
+    "valueField": "close"
+  } ],
+  "chartScrollbar": {
+    "graph": "g1",
+    "graphType": "line",
+    "scrollbarHeight": 30
+  },
+  "chartCursor": {
+    "valueLineEnabled": true,
+    "valueLineBalloonEnabled": true
+  },
+  "categoryField": "date",
+  "categoryAxis": {
+    "parseDates": true
+  },
+  "dataProvider": [ {
+    "date": "2011-08-01",
+    "open": "136.65",
+    "high": "136.96",
+    "low": "134.15",
+    "close": "136.49"
+  }, {
+    "date": "2011-08-02",
+    "open": "135.26",
+    "high": "135.95",
+    "low": "131.50",
+    "close": "131.85"
+  }, {
+    "date": "2011-08-05",
+    "open": "132.90",
+    "high": "135.27",
+    "low": "128.30",
+    "close": "135.25"
+  }, {
+    "date": "2011-08-06",
+    "open": "134.94",
+    "high": "137.24",
+    "low": "132.63",
+    "close": "135.03"
+  }, {
+    "date": "2011-08-07",
+    "open": "136.76",
+    "high": "136.86",
+    "low": "132.00",
+    "close": "134.01"
+  }, {
+    "date": "2011-08-08",
+    "open": "131.11",
+    "high": "133.00",
+    "low": "125.09",
+    "close": "126.39"
+  }, {
+    "date": "2011-08-09",
+    "open": "123.12",
+    "high": "127.75",
+    "low": "120.30",
+    "close": "125.00"
+  }, {
+    "date": "2011-08-12",
+    "open": "128.32",
+    "high": "129.35",
+    "low": "126.50",
+    "close": "127.79"
+  }, {
+    "date": "2011-08-13",
+    "open": "128.29",
+    "high": "128.30",
+    "low": "123.71",
+    "close": "124.03"
+  } ],
+
+  "export": {
+    "enabled": false
+  }
+} );
+
+chart.addListener( "rendered", zoomChart );
+zoomChart();
+
+// this method is called when chart is first inited as we listen for "dataUpdated" event
+function zoomChart() {
+  // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
+  chart.zoomToIndexes( chart.dataProvider.length - 10, chart.dataProvider.length - 1 );
+}
 }
 });
